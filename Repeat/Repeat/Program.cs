916fe614e7846857ea3ed82428110b9d98311ea2
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-20 09:59:46 +0200

    #1 | Aufgabe 1: initialisiere 2 Zahlen und gebe die Summe der Zahlen auf der Console aus

---
+++ Repeat/Repeat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatUebung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int z1 = 5;
+            int z2 = 10;
+            double ergebnis = z1 + z2;
+            Console.WriteLine(ergebnis);
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
